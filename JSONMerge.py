302e73831c892df7ddeb7a69117967bd8a91901d
proalphaGrossM <36879175+proalphaGrossM@users.noreply.github.com>
2018-03-14 11:09:55 +0100

    Create JSONMerge.py

---
+++ JSONMerge.py
@@ -0,0 +1,36 @@
+import json, os
+
+# define the file names for the json files to read here
+# the first file must be the one with all keys because
+# the script iterates over them and searches for the
+# one with the same key in the second json file
+filename1 = "ttInstanzKatalog.json"
+filename2 = "ttObjektInhalt.json"
+resultFilename = "result.json"
+
+# define the key name by which the json should be merged
+# it may mostly be the same name in each json
+mergeKeyName1 = "cInstanzName"
+mergeKeyName2 = "cInstanzName"
+
+logFilename = "log.txt" 
+
+# creates log file
+log = open(logFilename, 'w')
+
+# Open the 2 json files and read its json
+instanzKatalogJson = json.load(open(filename1))
+objektInhaltJson = json.load(open(filename2))
+
+log.write("JSON files read successfully\n")
+
+for item in instanzKatalogJson["ttInstanzKatalog"]:
+	for item2 in objektInhaltJson["ttObjektInhalt"]:  
+		if item[mergeKeyName1] == item2[mergeKeyName2]:
+			log.write("combining " + item[mergeKeyName1] +"\n")
+			item.update(item2)
+
+f = open(resultFilename, 'w')
+json.dump(instanzKatalogJson, f)
+f.close()
+log.close()
